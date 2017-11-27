8b807bd9c732aae00b59746f7d08796b2545baca
Giusseppe <giusseppe.bustamante@arrow-gs.com>
2017-11-27 12:48:18 -0600

    pdf

--- src/main/java/com/arrowgs/agsadmin/api/ProductApi.java
+++ src/main/java/com/arrowgs/agsadmin/api/ProductApi.java
@@ -279,26 +279,26 @@ public class ProductApi {
 							result.setProduct(product);
 							result.setUrl(imageName);							
 							
-							BufferedImage imageBuf = ImageIO.read(finalFile);
-							
-							File compressedImageFile = new File(ImagePropertiesHelper.resource()+"/compressed."+content);
-							OutputStream os = new FileOutputStream(compressedImageFile);
-							
-							Iterator<ImageWriter> writers = ImageIO.getImageWritersByFormatName(content);
-							ImageWriter writer = (ImageWriter) writers.next();
-							
-							ImageOutputStream ios = ImageIO.createImageOutputStream(os);
-						    writer.setOutput(ios);
-							
-						    ImageWriteParam param = writer.getDefaultWriteParam();
-						    
-						    param.setCompressionMode(ImageWriteParam.MODE_EXPLICIT);
-						    param.setCompressionQuality(0.05f);
-						    writer.write(null, new IIOImage(imageBuf, null, null), param);
-						      
-						    os.close();
-						    ios.close();
-						    writer.dispose();						    
+//							BufferedImage imageBuf = ImageIO.read(finalFile);
+//							
+//							File compressedImageFile = new File(ImagePropertiesHelper.resource()+"/compressed."+content);
+//							OutputStream os = new FileOutputStream(compressedImageFile);
+//							
+//							Iterator<ImageWriter> writers = ImageIO.getImageWritersByFormatName(content);
+//							ImageWriter writer = (ImageWriter) writers.next();
+//							
+//							ImageOutputStream ios = ImageIO.createImageOutputStream(os);
+//						    writer.setOutput(ios);
+//							
+//						    ImageWriteParam param = writer.getDefaultWriteParam();
+//						    
+//						    param.setCompressionMode(ImageWriteParam.MODE_EXPLICIT);
+//						    param.setCompressionQuality(0.05f);
+//						    writer.write(null, new IIOImage(imageBuf, null, null), param);
+//						      
+//						    os.close();
+//						    ios.close();
+//						    writer.dispose();						    
 						    
 						    productService.addProductDetail(result);
 							status = ResponseStatus.OK;
