03960a8997b9cfbb923692a59df7a58299ebccbd
hugo camargo <hugo.camargo@arrow-gs.com>
2018-02-02 17:02:28 -0600

    casi terminado

--- src/main/webapp/r/js/dev/app.js
+++ src/main/webapp/r/js/dev/app.js
@@ -1,12 +1,12 @@
 angular.module( appname+'App', ['ui.bootstrap','rt.select2'])
 .config([appname+'ServiceProvider', function(ServiceProvider){
 	//ServiceProvider.setUrl('/'+appname+'/api');
-	ServiceProvider.setUrl('/ags-admin/rest');
+	ServiceProvider.setUrl('/rest');
 }])
 
 
 var app = angular.module( appname+'App',  ['ui.bootstrap','rt.select2']);
 app.config([appname+'ServiceProvider', function(ServiceProvider){
 	//ServiceProvider.setUrl('/'+appname+'/api');
-	ServiceProvider.setUrl('/ags-admin/rest');
+	ServiceProvider.setUrl('/rest');
 }])
