ecf82b1414d78a71d6819b944c210ef892c2934d
hugo camargo <hugo.camargo@arrow-gs.com>
2018-03-06 18:15:33 -0600

    MAster of desaster

--- src/main/webapp/r/js/dev/app.js
+++ src/main/webapp/r/js/dev/app.js
@@ -2,6 +2,7 @@ angular.module( appname+'App', ['ui.bootstrap','rt.select2'])
 .config([appname+'ServiceProvider', function(ServiceProvider){
 	//ServiceProvider.setUrl('/'+appname+'/api');
 	ServiceProvider.setUrl('/ags-admin/rest');
+	//ServiceProvider.setUrl('/rest');
 }])
 
 
@@ -9,4 +10,5 @@ var app = angular.module( appname+'App',  ['ui.bootstrap','rt.select2']);
 app.config([appname+'ServiceProvider', function(ServiceProvider){
 	//ServiceProvider.setUrl('/'+appname+'/api');
 	ServiceProvider.setUrl('/ags-admin/rest');
+	//ServiceProvider.setUrl('/rest');
 }])
