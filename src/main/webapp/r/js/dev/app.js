531ecc2745ed590e8df397ce57419d524060497e
Giusseppe <giusseppe.bustamante@arrow-gs.com>
2018-04-12 18:08:26 -0500

    Server

--- src/main/webapp/r/js/dev/app.js
+++ src/main/webapp/r/js/dev/app.js
@@ -1,14 +1,14 @@
 angular.module( appname+'App', ['ui.bootstrap','rt.select2'])
 .config([appname+'ServiceProvider', function(ServiceProvider){
 	//ServiceProvider.setUrl('/'+appname+'/api');
-	ServiceProvider.setUrl('/ags-admin/rest');
-//	ServiceProvider.setUrl('/rest');
+//	ServiceProvider.setUrl('/ags-admin/rest');
+	ServiceProvider.setUrl('/admin/rest');
 }])
 
 
 var app = angular.module( appname+'App',  ['ui.bootstrap','rt.select2']);
 app.config([appname+'ServiceProvider', function(ServiceProvider){
 	//ServiceProvider.setUrl('/'+appname+'/api');
-	ServiceProvider.setUrl('/ags-admin/rest');
-//	ServiceProvider.setUrl('/rest');
+//	ServiceProvider.setUrl('/ags-admin/rest');
+	ServiceProvider.setUrl('/admin/rest');
 }])
